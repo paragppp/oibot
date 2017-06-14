e17ced6f183082fb03aafefef0cc28273a0fd57c
Parag Paithankar <parag.paithankar@gmail.com>
2017-06-14 22:29:48 +0900

    Added Node code

---
+++ app.js
@@ -0,0 +1,20 @@
+﻿// Builder
+var builder = require('botbuilder');
+
+// Connector Type: Console
+var connector = new builder.ConsoleConnector().listen();
+
+// Bot
+var bot = new builder.UniversalBot(connector);
+
+// Dialog
+bot.dialog('/', [
+    // Ask Name
+    function (session) {
+        builder.Prompts.text(session, "Hi, what's your name?");
+    },
+    // Greet by name
+    function (session, results) {
+        session.send("Hello how are you today?", results.response);   
+    } 
+]);
